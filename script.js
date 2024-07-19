0d7fccda880f5437805fcb6404131771666ac8bb
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2024-07-19 13:33:18 +0530

    fixed the callback code

--- script.js
+++ script.js
@@ -1,48 +1,87 @@
 const countHeading = document.getElementById("countDown");
 const btn = document.getElementById("countButton");
 
-function countDown(countDec, callback) {
-  setTimeout(() => {
-    let count = countDec - 1;
-    if (count >= 1) {
-      callback(count);
-    } else {
-      callback("Happy Independence Day!");
-    }
-  }, 1000);
-}
-
 btn.onclick = function () {
   let count = 10;
   countHeading.innerHTML = count;
-  countDown(count, function (count) {
-    countHeading.innerHTML = count;
-    countDown(count, function (count) {
+  setTimeout(() => {
+    count = count - 1;
+    if (count >= 1) {
       countHeading.innerHTML = count;
-      countDown(count, function (count) {
+    } else {
+      countHeading.innerHTML = "Happy Independence Day";
+    }
+    setTimeout(() => {
+      count = count - 1;
+      if (count >= 1) {
         countHeading.innerHTML = count;
-        countDown(count, function (count) {
+      } else {
+        countHeading.innerHTML = "Happy Independence Day";
+      }
+      setTimeout(() => {
+        count = count - 1;
+        if (count >= 1) {
           countHeading.innerHTML = count;
-          countDown(count, function (count) {
+        } else {
+          countHeading.innerHTML = "Happy Independence Day";
+        }
+        setTimeout(() => {
+          count = count - 1;
+          if (count >= 1) {
             countHeading.innerHTML = count;
-            countDown(count, function (count) {
+          } else {
+            countHeading.innerHTML = "Happy Independence Day";
+          }
+          setTimeout(() => {
+            count = count - 1;
+            if (count >= 1) {
               countHeading.innerHTML = count;
-              countDown(count, function (count) {
+            } else {
+              countHeading.innerHTML = "Happy Independence Day";
+            }
+            setTimeout(() => {
+              count = count - 1;
+              if (count >= 1) {
                 countHeading.innerHTML = count;
-                countDown(count, function (count) {
+              } else {
+                countHeading.innerHTML = "Happy Independence Day";
+              }
+              setTimeout(() => {
+                count = count - 1;
+                if (count >= 1) {
                   countHeading.innerHTML = count;
-                  countDown(count, function (count) {
+                } else {
+                  countHeading.innerHTML = "Happy Independence Day";
+                }
+                setTimeout(() => {
+                  count = count - 1;
+                  if (count >= 1) {
                     countHeading.innerHTML = count;
-                    countDown(count, function (count) {
+                  } else {
+                    countHeading.innerHTML = "Happy Independence Day";
+                  }
+                  setTimeout(() => {
+                    count = count - 1;
+                    if (count >= 1) {
                       countHeading.innerHTML = count;
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-};
+                    } else {
+                      countHeading.innerHTML = "Happy Independence Day";
+                    }
+                    setTimeout(() => {
+                      count = count - 1;
+                      if (count >= 1) {
+                        countHeading.innerHTML = count;
+                      } else {
+                        countHeading.innerHTML = "Happy Independence Day";
+                      }
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}
